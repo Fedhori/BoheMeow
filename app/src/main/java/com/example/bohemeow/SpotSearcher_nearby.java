f76136cd1e10b18c4363876935b324d8c73ad88f
LJW25 <dbjwlee@gmail.com>
2020-07-02 12:29:35 +0900

    add place api

---
+++ app/src/main/java/com/example/bohemeow/SpotSearcher_nearby.java
@@ -0,0 +1,18 @@
+package com.example.bohemeow;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpotSearcher_nearby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
