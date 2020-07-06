4b769e18f980dbf408c8cf80007b8c113f2b9281
Edward Kim <ed991015@naver.com>
2020-07-06 15:20:35 +0900

    초기 로딩화면 + 메인 화면 + 선택 화면 구현중

--- app/src/main/java/com/example/bohemeow/MainActivity.java
+++ app/src/main/java/com/example/bohemeow/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.bohemeow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.drm.DrmStore;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.loadingLayout);
+
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(intent);
+            }
+        });
 
         //hello world
 
---
+++ app/src/main/java/com/example/bohemeow/MainMenu.java
@@ -0,0 +1,29 @@
+package com.example.bohemeow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Intent intent = getIntent();
+
+        Button selectBtn = (Button) findViewById(R.id.selectBtn);
+        selectBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, SelectActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
