f4d39a1197d31a2fbd33e4caef5779b2eaf2e90b
Edward Kim <ed991015@naver.com>
2020-07-14 14:03:18 +0900

    경로 출력 및 마커 표시 구현 완료

--- app/src/main/java/com/example/bohemeow/WalkActivity.java
+++ app/src/main/java/com/example/bohemeow/WalkActivity.java
@@ -1,17 +1,81 @@
 package com.example.bohemeow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
+import com.skt.Tmap.TMapView;
+import com.skt.Tmap.TMapData.TMapPathType;
+import com.skt.Tmap.TMapData.FindPathDataListenerCallback;
 
 public class WalkActivity extends AppCompatActivity {
+
+    private TMapView tMapView = null;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
+        context = this;
 
         Intent intent = getIntent();
+
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+
+        tMapView = new TMapView(this);
+        tMapView.setSKTMapApiKey("l7xxc4527e777ef245ef932b366ccefaa9b0");
+        linearLayoutTmap.addView( tMapView );
+
+        // set center point
+        tMapView.setCenterPoint(126.97406798055658, 37.29389181202027);
+
+        // draw marker
+        TMapPoint tMapPoint1 = new TMapPoint(37.29389181204, 126.97406798057);
+        drawMarker(tMapPoint1);
+
+        // draw route
+        TMapPoint startPoint = new TMapPoint(37.2939299 , 126.9739263);
+        TMapPoint endPoint = new TMapPoint(37.283337, 126.982062);
+        drawPedestrianPath(startPoint, endPoint);
+    }
+
+    public void drawPedestrianPath(TMapPoint startPoint, TMapPoint endPoint) {
+
+        TMapData tmapdata = new TMapData();
+
+        tmapdata.findPathDataWithType(TMapPathType.PEDESTRIAN_PATH, startPoint, endPoint, new FindPathDataListenerCallback() {
+            @Override
+            public void onFindPathData(TMapPolyLine polyLine) {
+                polyLine.setLineColor(Color.BLUE);
+                tMapView.addTMapPath(polyLine);
+            }
+        });
+    }
+
+    public void drawMarker(TMapPoint position){
+        // get bitmap
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.main_cat_scaratch);
+        // resize bitmap
+        bitmap = Bitmap.createScaledBitmap(bitmap, 120, 120, false);
+
+        TMapMarkerItem markerItem1 = new TMapMarkerItem();
+        markerItem1.setIcon(bitmap); // 마커 아이콘 지정
+        markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+        markerItem1.setTMapPoint(position); // 마커의 좌표 지정
+        markerItem1.setName("성대");
+        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
     }
 }
