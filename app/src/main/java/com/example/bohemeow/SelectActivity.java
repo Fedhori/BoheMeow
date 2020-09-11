8d5afc935199ee10a13431d15bb8ee75005f0a7e
LJW25 <dbjwlee@gmail.com>
2020-09-11 15:13:49 +0900

    modify select activity

--- app/src/main/java/com/example/bohemeow/SelectActivity.java
+++ app/src/main/java/com/example/bohemeow/SelectActivity.java
@@ -9,6 +9,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,23 +20,60 @@ import com.google.firebase.database.ValueEventListener;
 
 public class SelectActivity extends AppCompatActivity {
 
-    Button btn_to_walk;
+    int time = 30;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select);
+        setContentView(R.layout.activity_select_2);
+
+        text = findViewById(R.id.time_view);
 
         Intent intent = getIntent();
 
-        btn_to_walk = (Button) findViewById(R.id.btn_to_walk);
-
-        btn_to_walk.setOnClickListener(new View.OnClickListener() {
+        ImageButton sub_btn = findViewById(R.id.sub_btn);
+        sub_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SelectActivity.this, WalkLoadingActivity.class);
+                if(time > 10) {
+                    time -= 10;
+                    text.setText(Integer.toString(time) + "분");
+                }
+            }
+        });
+
+        ImageButton add_btn = findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(time < 180) {
+                    time += 10;
+                    text.setText(Integer.toString(time) + "분");
+                }
+            }
+        });
+
+        ImageButton back_btn = findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectActivity.this, MainMenu.class);
                 startActivity(intent);
             }
         });
+
+        ImageButton start_btn = findViewById(R.id.start_btn);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectActivity.this, WalkLoadingActivity.class);
+                intent.putExtra("time", time);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
