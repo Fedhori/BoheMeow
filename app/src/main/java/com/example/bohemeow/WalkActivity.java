7b3fa1dc719b0d6e4baaee8da67aaca0d77b4dc3
Edward Kim <ed991015@naver.com>
2020-09-29 18:53:16 +0900

    보물찾기 로그 형식 변경

--- app/src/main/java/com/example/bohemeow/WalkActivity.java
+++ app/src/main/java/com/example/bohemeow/WalkActivity.java
@@ -170,8 +170,6 @@ public class WalkActivity extends AppCompatActivity implements onLocationChanged
         nickname = registerInfo.getString("registerUserName", "NULL");
         phoneNumber = registerInfo.getString("phoneNumber", "NULL");
 
-        findLots();
-
         walkStartTime = System.currentTimeMillis();
 
         // get intent
