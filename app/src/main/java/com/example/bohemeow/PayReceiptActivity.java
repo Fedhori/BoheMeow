7d53ca7a5fbc9e3a4cdf85aa800fdfe5e5dd4948
LJW25 <dbjwlee@gmail.com>
2020-10-30 02:33:44 +0900

    edit pay UI

--- app/src/main/java/com/example/bohemeow/PayReceiptActivity.java
+++ app/src/main/java/com/example/bohemeow/PayReceiptActivity.java
@@ -18,17 +18,24 @@ public class PayReceiptActivity extends Activity {
         setContentView(R.layout.activity_pay_receipt);
 
 
-        TextView storeNameTV = findViewById(R.id.textView30);
-        TextView timeTV = findViewById(R.id.textView31);
-        TextView point_useTV = findViewById(R.id.textView25);
-        TextView point_remainTV = findViewById(R.id.textView29);
+        TextView storeNameTV = findViewById(R.id.storeName);
+        TextView timeTV = findViewById(R.id.textView35);
+        TextView point_useTV = findViewById(R.id.textView36);
+        TextView point_remainTV = findViewById(R.id.textView37);
 
         final Intent intent = getIntent();
 
-        storeNameTV.setText("매장명 :  "  + intent.getStringExtra("storeName"));
-        timeTV.setText("거래 일시 :  " + intent.getStringExtra("time"));
-        point_useTV.setText("결제 금액 :  " + intent.getStringExtra("point_use"));
-        point_remainTV.setText("잔여 포인트 :  " + intent.getStringExtra("point_remain"));
+
+        String store = intent.getStringExtra("storeName");
+        if(store.equals("NU")) store = "Cafe NU:";
+        else if (store.equals("Pandorothy")) store = "팬도로시";
+
+        String time = intent.getStringExtra("time");
+
+        storeNameTV.setText(store);
+        timeTV.setText(time.substring(0, 10) + "\n" + time.substring(11,19));
+        point_useTV.setText(intent.getStringExtra("point_use"));
+        point_remainTV.setText(intent.getStringExtra("point_remain"));
 
 
         Button btn = findViewById(R.id.button2);
