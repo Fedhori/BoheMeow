6dcc380110b485625380a13a4265a3a6559db045
LJW25 <dbjwlee@gmail.com>
2020-10-29 19:37:44 +0900

    modify weight config

--- app/src/main/java/com/example/bohemeow/ConfigEditWeightActivity.java
+++ app/src/main/java/com/example/bohemeow/ConfigEditWeightActivity.java
@@ -9,11 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ConfigEditWeightActivity extends Activity {
 
+    EditText newWeight;
     int weight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +38,55 @@ public class ConfigEditWeightActivity extends Activity {
             }
         });
 
-        final EditText newWeight = findViewById(R.id.newWeight);
-        weight = Integer.parseInt(newWeight.getText().toString());
+        getUserData(userData.nickname);
+
+        newWeight = findViewById(R.id.newWeight);
+        //newWeight.setText(""+weight);
+
+
 
         Button change_btn = findViewById(R.id.yes_btn);
         change_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                if(weight > 20 && weight < 200){
+                weight = Integer.parseInt(newWeight.getText().toString());
+                if(weight > 25 && weight < 200){
                     DatabaseReference ref = FirebaseDatabase.getInstance().getReference("user_list").child(userData.nickname);
-                    ref.child("weight").setValue(Integer.valueOf(newWeight.getText().toString()));
+                    ref.child("weight").setValue(Integer.valueOf(weight));
 
                     Toast.makeText(ConfigEditWeightActivity.this, "체중 변경이 완료되었습니다.", Toast.LENGTH_SHORT).show();
                     setResult(RESULT_OK);
                     finish();
                 }
                 else{
-                    Toast.makeText(ConfigEditWeightActivity.this, "정확한 체중을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ConfigEditWeightActivity.this, "실제 체중을 입력해 주세요.", Toast.LENGTH_SHORT).show();
                 }
+
+            }
+        });
+    }
+
+    private DatabaseReference mPostReference;
+    public void getUserData(final String user_nickname){
+        mPostReference = FirebaseDatabase.getInstance().getReference();
+        mPostReference.child("user_list").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    UserData get = postSnapshot.getValue(UserData.class);
+
+                    if(user_nickname.equals(get.nickname)){
+                        weight = get.weight;
+                        newWeight.setText(""+weight);
+                        //Toast.makeText(WalkActivity.this, preference[0] + " " + preference[1] + " " + preference[2], Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
