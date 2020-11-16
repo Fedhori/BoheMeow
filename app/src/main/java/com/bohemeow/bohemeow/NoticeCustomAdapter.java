ea06f442eea918708ce1faacfcc66e4a6f79ed30
LEEJIWOO <dbjwlee@gmail.com>
2020-11-17 01:16:18 +0900

    change notice image

--- app/src/main/java/com/bohemeow/bohemeow/NoticeCustomAdapter.java
+++ app/src/main/java/com/bohemeow/bohemeow/NoticeCustomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -47,11 +48,16 @@ public class NoticeCustomAdapter extends BaseAdapter {
         TextView titleText = view.findViewById(R.id.titleText);
         TextView contentText = view.findViewById(R.id.contentText);
         TextView authorText = view.findViewById(R.id.authorText);
+        ImageView catType = view.findViewById(R.id.imageView45);
+
+        int[] icons = {R.drawable.cathead_null, R.drawable.hanggangic, R.drawable.bameeic, R.drawable.chachaic,
+                R.drawable.ryoniic, R.drawable.moonmoonic, R.drawable.popoic,R.drawable.taetaeic, R.drawable.sessakic};
 
         timeText.setText(noticeData.get(position).time);
         titleText.setText(noticeData.get(position).title);
         contentText.setText(noticeData.get(position).content);
         authorText.setText(noticeData.get(position).author);
+        catType.setImageResource(icons[noticeData.get(position).catType]);
 
         return view;
     }
